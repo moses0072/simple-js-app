dc270104f503f7252f92bca3939d94b0b2a6df3d
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-03-14 18:09:33 +0100

    Starting a javaScript Project from scratch
    Deleting the Hello World and pizza code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-//Variable pizza
-let favoriteFood = 'Pizza';
-//popup Hello World
-alert('Hello World');
-document.write(favoriteFood);
+//Array contains objects of Pokomon data 
+let pokemonList = [
+    {name:'Bulbasaur' , height:7 , types:['grass', 'poison']},
+    {name:'Charizad' , height:5 , types: ['fire' , 'flying']},
+    {name: 'Butterfree', height:3 , types: ['bug' , 'flying']}
+];
