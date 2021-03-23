0998ad99f5fccffdf81836fe499d68f245fe370d
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-03-23 23:05:21 +0100

    Manipolated the DOM. Added list item and a butten for each displayed pokemon.  then create an event listener on click, it displayed the information of the clicked pokemon on the console

--- js/script.js
+++ js/script.js
@@ -1,51 +1,69 @@
-//IIFE 
+
+//IIFE
 let pokemonRepository = (function(){
-//Array contains objects of Pokomon data    
-let pokemonList = [
-    {
-        name:'Bulbasaur',
-        height:7, 
-        types:['grass', 'poison']
-    },
-    {
-        name:'Charizad' , 
-        height:5, 
-        types: ['fire' , 'flying']
-    },
-    {
-        name: 'Butterfree', 
-        height:3 , 
-        types: ['bug' , 'flying']
-    }
-];
-//getAll execute the pokemonlist
-function getAll(){
+    //Array contains objects of pokemon
+    let pokemonList = [
+      {
+          name:'Bulbasaur',
+          height:7, 
+          types:['grass', 'poison']
+      },
+      {
+          name:'Charizad' , 
+          height:5, 
+          types: ['fire' , 'flying']
+      },
+      {
+          name: 'Butterfree', 
+          height:3 ,
+          types: ['bug' , 'flying']
+      }
+  ];
+  //creating lists and button in the DOM
+  function addListItem(pokemon){
+  let pokList = document.querySelector('.pokemon-list');
+  let listItem = document.createElement('li');
+  let button = document.createElement('button');
+  //pokemon names on the buttons  
+  button.innerText = pokemon.name;
+  button.classList.add('button-class');
+  //append the butten und the list to thier parents  
+  listItem.appendChild(button);
+  pokList.appendChild(listItem);
+  //Event listener on click
+  button.addEventListener('click', function(event){
+        showDetails(pokemon);
+    });
+  }
+  //execute the details of clicked pokemon on console 
+  function showDetails(pokemon){
+      console.log(pokemon);
+  }
+  //getAll execute the pokemonlist
+  function getAll(){
     return pokemonList;
   }
+  //checking the type of the variable pokemonRepository
   //pushes items into the pokemonList
   function add(item){
+    if (typeof item === 'object'){
     pokemonList.push(item);
+    } 
   }
-
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   }
-  })();
-//adding a new object to the pokemonList
-pokemonRepository.add({name: 'pikachu', height: 1});
-//forEach function to display the contant of the PokemonList array
-let text = ' -- Wow, that\'s big! ';
-pokemonRepository.getAll().forEach(function(list){
-    if (list.height > 5){
-        document.write('<strong>', list.name + ' ' + '(height: ' + list.height +') ' + ' ' + list.types[0] + ' ' + list.types[1] + ' ' + text, '</strong>','<br>');
-    } else if (list.height > 4 && list.height < 6){
-        document.write('<strong>', list.name + ' ' + '(height: ' + list.height + ') ','</strong>','<br>');
-    } else {
-        document.write('<strong>', list.name + ' ' + '(height: ' + list.height + ')','</strong>','<br>');
-    };
-    
-});
-//checking the type of the variable pokemonRepository
-document.write(typeof pokemonRepository.getAll());
-
+    })();
+  //adding a new object to the pokemonList  
+  pokemonRepository.add({name: 'pikachu', height: 1});
+  
+  //forEach loop to display the contant of the PokemonList array
+  
+  let text = ' -- Wow, that\'s big! '; 
+     pokemonRepository.getAll().forEach(function(pokemon){
+      
+      pokemonRepository.addListItem(pokemon);
+    });
+  
