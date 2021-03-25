33fe23fb214dc88893f18b32f9f1de18b82c3e24
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-03-25 22:45:39 +0100

    Deleted the pokemon object from the array and fetched a list of arrys from an API. Creats 2 functions for loading one for objects and the other for details. Using promise. Execute the pokemonlist but first getet sure that the list is loaded

--- js/script.js
+++ js/script.js
@@ -1,69 +1,98 @@
 
 //IIFE
 let pokemonRepository = (function(){
-    //Array contains objects of pokemon
-    let pokemonList = [
-      {
-          name:'Bulbasaur',
-          height:7, 
-          types:['grass', 'poison']
-      },
-      {
-          name:'Charizad' , 
-          height:5, 
-          types: ['fire' , 'flying']
-      },
-      {
-          name: 'Butterfree', 
-          height:3 ,
-          types: ['bug' , 'flying']
-      }
-  ];
+    //Empty array will be filld with pokemon objects from an API
+    let pokemonList = [];
+  
+  //defined the API url in a variable
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
   //creating lists and button in the DOM
   function addListItem(pokemon){
   let pokList = document.querySelector('.pokemon-list');
   let listItem = document.createElement('li');
   let button = document.createElement('button');
+  
   //pokemon names on the buttons  
   button.innerText = pokemon.name;
   button.classList.add('button-class');
+  
   //append the butten und the list to thier parents  
   listItem.appendChild(button);
   pokList.appendChild(listItem);
+  
   //Event listener on click
   button.addEventListener('click', function(event){
         showDetails(pokemon);
     });
   }
-  //execute the details of clicked pokemon on console 
+
+  //execute the details of clicked pokemon as a promise on console 
   function showDetails(pokemon){
+    loadDetails(pokemon).then(function(){
       console.log(pokemon);
+    });
   }
+
   //getAll execute the pokemonlist
   function getAll(){
     return pokemonList;
   }
+
   //checking the type of the variable pokemonRepository
   //pushes items into the pokemonList
-  function add(item){
-    if (typeof item === 'object'){
-    pokemonList.push(item);
+  function add(pokemon){
+    if (typeof pokemon === 'object' &&
+    'name' in pokemon
+    ){
+    pokemonList.push(pokemon);
     } 
   }
+
+  //Get a list of pokemon from the API
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+//Getting the pokemon details from the pokemon object using the Url
+function loadDetails(item){
+  let url = item.detailsUrl;
+  return fetch(url).then(function(response){
+    return response.json();
+  }).then(function(details){
+    item.imageUrl = details.sprites.front_default;
+    item.height = details.height;
+    item.types = details.types;
+  }).catch(function (e){
+    console.error(e);
+  })
+}
+//executed funtion outside the IIFE
   return {
     add: add,
     getAll: getAll,
+    loadList: loadList,
+    loadDetails: loadDetails,
     addListItem: addListItem
-  }
+  };
     })();
-  //adding a new object to the pokemonList  
-  pokemonRepository.add({name: 'pikachu', height: 1});
-  
-  //forEach loop to display the contant of the PokemonList array
-  
-  let text = ' -- Wow, that\'s big! '; 
-     pokemonRepository.getAll().forEach(function(pokemon){
-      
+ 
+  //Calling the loadList function of pokemonrepository
+  pokemonRepository.loadList().then(function(){
+  //Executed the getAll function
+    pokemonRepository.getAll().forEach(function(pokemon){
       pokemonRepository.addListItem(pokemon);
     });
-  
+});
