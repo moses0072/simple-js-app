1d2d8470e109919e452718badc7eef7265390e74
Moussa Ghaddar <78150453+moses0072@users.noreply.github.com>
2021-03-28 21:29:00 +0200

    Createing a Modal to display the pokemon  details on it . The modal should close once the user on 'close button', outsite the modal or the escape key clicked, should the modal close.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,72 @@
 
 //IIFE
 let pokemonRepository = (function(){
-    //Empty array will be filld with pokemon objects from an API
-    let pokemonList = [];
+  let modalContainer = document.querySelector('#modal-container');
+  let pokList = document.querySelector('.pokemon-list');
+  //Empty array will be filld with pokemon objects from an API
+  let pokemonList = [];
   
   //defined the API url in a variable
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+ 
+  function showModal(pokemon) {
+    
+    modalContainer.innerHTML = '';
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+    modal.appendChild(closeButtonElement);
+
+    //get the name of the pokemon and display it on Modal
+    let pokName = document.createElement('h1');
+    pokName.innerText = pokemon.name;
+    modal.appendChild(pokName);
+
+    //Get the height of the pokemon and display it on the Modal
+    let pokHeight = document.createElement('p');
+    pokHeight.innerText = pokemon.height;
+    modal.appendChild(pokHeight);
+
+    //get the img of the pokemon and display it on the Modal  
+    let containerImg = document.querySelector('#image-container');
+    let pokImg = document.createElement('img');
+    pokImg.src = pokemon.imageUrl;
+    modal.appendChild(pokImg);
+
+
+    //modal is the child of modalContainer
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible'); 
+  }
+
+  //Hide function 
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  // close the Model when Escape key got hitted
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+  // Close the function when the user clicked outside the Modal
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  /*document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal();
+  });*/
 
   //creating lists and button in the DOM
   function addListItem(pokemon){
@@ -26,13 +87,15 @@ let pokemonRepository = (function(){
         showDetails(pokemon);
     });
   }
-
+  
   //execute the details of clicked pokemon as a promise on console 
   function showDetails(pokemon){
     loadDetails(pokemon).then(function(){
-      console.log(pokemon);
+    //Calling the Modal
+    showModal(pokemon);  
     });
   }
+
 
   //getAll execute the pokemonlist
   function getAll(){
@@ -57,7 +120,8 @@ let pokemonRepository = (function(){
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.url
+          detailsUrl: item.url,
+          //height: item.height
         };
         add(pokemon);
       });
@@ -79,6 +143,7 @@ function loadDetails(item){
     console.error(e);
   })
 }
+
 //executed funtion outside the IIFE
   return {
     add: add,
